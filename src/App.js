2fa071431c9493a8a7eb1dd7b55985cc63cd49c6
ModeEric <ericjm4@illinois.edu>
2023-07-25 16:05:34 -0400

    build project and fix app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import Weather from './components/Weather';
+import Mood from './components/Mood';
+import Activities from './components/Activities';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Self-Care Dashboard</h1>
       </header>
+      <main>
+        <Weather />
+        <Mood />
+        <Activities />
+      </main>
+      <footer>
+        {/* Any footer information you'd like */}
+      </footer>
     </div>
   );
 }
